23305b49506da73517afd8e57af15cd8d0f8b3f1
Foysal-Ahmmed <pmfoysal@gmail.com>
2021-07-04 18:55:27 +0600

    Home Page Completed

--- assets/scripts/custom.js
+++ assets/scripts/custom.js
@@ -1,11 +1,25 @@
 let headerSearchButton = document.getElementById('header__search--icon');
 let headerSearchBox = document.getElementById('header__search--box');
 
-headerSearchButton.addEventListener('click', function() {
+headerSearchButton.addEventListener('click', function () {
     if (headerSearchBox.classList == 'search-close') {
         headerSearchBox.classList.replace('search-close', 'search-open');
-    }
-    else {
+    } else {
         headerSearchBox.classList.replace('search-open', 'search-close');
+    }
+});
+
+// ======================================== \\
+
+const header2 = document.querySelector('#header2');
+const header2Brand = document.querySelector('#header2__brand');
+const headerGap = header2.offsetTop;
+window.addEventListener('scroll', () => {
+    if (headerGap <= window.scrollY) {
+        header2.classList.add('header_sticky');
+        header2Brand.classList.add('header2__brand_op');
+    } else {
+        header2.classList.remove('header_sticky');
+        header2Brand.classList.remove('header2__brand_op');
     }
 })
